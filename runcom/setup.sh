6c973d82b963179ede2b83b4cf8ecd35ed824452
Maciej Gibas <gibasmaciej@gmail.com>
2019-05-28 07:15:55 +0200

    Fixed directories

---
+++ runcom/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp .bash_profile ~/.bash_profile
+cp .prompt_profile ~/.prompt_profile
+cp .inputrc ~/.inputrc
--- setup.sh
+++ setup.sh
@@ -6,15 +6,32 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-#Bash profile
-cp ./runcom/.bash_profile ~/.bash_profile
-cp ./runcom/.prompt_profile ~/.prompt_profile
-cp ./runcom/.inputrc ~/.inputrc
-
 source ./install.sh
+
+cd ./runcom
+source ./runcom/setup.sh
+cd -
+
+cd ./git
 source ./git/setup.sh
+cd -
+
+cd ./macos
 source ./macos/setup.sh 
+cd -
+
+cd ./vim
 source ./vim/setup.sh
+cd -
+
+cd ./nginx
 source ./nginx/setup.sh
+cd -
+
+cd ./sketchapp
 source ./sketchapp/setup.sh
+cd -
+
+cd ./dir
 source ./dir/setup.sh 
+cd -
